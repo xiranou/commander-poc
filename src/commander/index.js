2ad21630a314fe16e4b7f3a929a9ba967bb33a0b
xiranou <xiranou@gmail.com>
2017-01-10 11:36:58 -0500

    with co

--- src/commander/index.js
+++ src/commander/index.js
@@ -1,22 +1,22 @@
+const co = require('co');
 const parser = require('../parser');
 const auth = require('../auth');
 const chat = require('../chat');
 
 function run(payload) {
-  const { type: payloadType } = payload;
-  const parsedPayload = parser[payloadType].parse(payload);
-  const chatContext = { userID, roomID } = parsedPayload.toJS();
+  return co(function* _run() {
+    const { type: payloadType } = payload;
+    const parsedPayload = parser[payloadType].parse(payload);
+    const chatContext = { userID, roomID } = parsedPayload.toJS();
 
-  chat.api.say('fetching user permission...', chatContext);
+    chat.api.say('fetching user permission...', chatContext);
 
-  auth.fetchUserPermissionByID(parsedPayload.userID).then(permission => {
+    const permission = yield auth.fetchUserPermissionByID(parsedPayload.userID);
     const runMeta = parsedPayload.merge(permission);
 
     chat.api.say('command in progress...', chatContext);
 
-    return commandHandler(runMeta);
-  })
-  .then(meta => {
+    const meta = yield commandHandler(runMeta);
     const message = meta.success ? 'command is successful' : 'command is uneventful';
 
     chat.api.say(message, chatContext);
